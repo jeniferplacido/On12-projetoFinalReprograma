e355c2445c6cd52e3e3197cb6e87a98c97802fb8
jeniferplacido <jenifer.sdti@gmail.com>
2021-09-10 13:53:41 -0300

    Atualizar rota

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const cors = require ('cors')
 const db = require ('./data/database')
 const app = express();
-const candidatas = require("./routes/candidatas.routes");
+const poderosas= require("./routes/candidatas.routes");
 db.connect();
 app.use(express.json());
 
 app.use(cors());
-app.use("/candidatas", candidatas);
+app.use("/poderosas", poderosas);
 
 
 module.exports = app;
