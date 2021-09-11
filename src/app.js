4ebd15d7c1808d48745d06883a09d2bde72e2932
jeniferplacido <jenifer.sdti@gmail.com>
2021-09-10 21:01:28 -0300

    Autenticação Usuária

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require ('cors')
 const db = require ('./data/database')
 const app = express();
-const poderosas = require("./routes/candidatas.routes");
+const poderosas = require("./routes/candidatas.routes")
+const usuaria = require('../src/routes/usuaria.routes')
 
 db.connect();
 app.use(express.json());
 
 app.use(cors());
 app.use("/poderosas", poderosas);
-
+app.use("/usuaria", usuaria)
 
 module.exports = app;
